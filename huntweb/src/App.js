b4e8d41539a666c4b16fbd0b569da25e5f032195
bruno <bruno@yoagencia.digital>
2019-11-26 14:18:46 -0300

    Projeto ReactJS

---
+++ huntweb/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import './styles.css';
+import Routes from './routes';
+
+const App = () => (
+  <div className='App'>
+    <Header/>
+    <Routes/>
+  </div>
+);
+
+export default App;
